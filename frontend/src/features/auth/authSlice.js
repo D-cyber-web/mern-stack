66a14c909fa085c66122813ed4320db3fec8dfc3
D-cyber-web <muthama3david@gmail.com>
2023-11-29 00:32:33 +0300

    created frontend

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,26 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+//get user from localStorage
+const user = JSON.parse(localStorage.getItem('user'))
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const authSlice = createSlice ({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isError = false
+            state.isLoading = false
+            state.isSuccess = false
+            state.message = ''
+        }
+    },
+    extraReducers: () => {}
+})
