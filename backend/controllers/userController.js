aaffde769cdaaef4171c93c650f5a3e8edbe538e
D-cyber-web <muthama3david@gmail.com>
2023-12-02 00:13:17 +0300

    updated mern-stack

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,4 +21,15 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('User already exists')
   }
+
+   // Hash password
+   const salt = await bcrypt.genSalt(10)
+   const hashedPassword = await bcrypt.hash(password, salt)
+ 
+   // Create user
+   const user = await User.create({
+     name,
+     email,
+     password: hashedPassword,
+   })
 })
