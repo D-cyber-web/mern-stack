1d652974d0a58c9dfd76bcbfa08861b85348e411
D-cyber-web <muthama3david@gmail.com>
2023-12-25 23:01:13 +0300

    updated mern-stack

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,7 @@ const connectDB = async () => {
     try {
         const conn = await mogoose.connect(process.env.MONGO_URI)
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.
-        underline)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     }catch (error) {
         console.log(error)
         process.exit(1)
