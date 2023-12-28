daf2e9cacb1b6befa1abea67dedb647a3b6e4af0
D-cyber-web <muthama3david@gmail.com>
2023-12-28 23:42:05 +0300

    updated mern-stack

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,40 +1,18 @@
-import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { createGoal } from '../features/goals/goalSlice'
+import { deleteGoal } from '../features/goals/goalSlice'
 
-function GoalForm() {
-  const [text, setText] = useState('')
-
+function GoalItem({ goal }) {
   const dispatch = useDispatch()
 
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    dispatch(createGoal({ text }))
-    setText('')
-  }
-
   return (
-    <section className='form'>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='text'>Goal</label>
-          <input
-            type='text'
-            name='text'
-            id='text'
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </div>
-        <div className='form-group'>
-          <button className='btn btn-block' type='submit'>
-            Add Goal
-          </button>
-        </div>
-      </form>
-    </section>
+    <div className='goal'>
+      <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
+      <h2>{goal.text}</h2>
+      <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
+        X
+      </button>
+    </div>
   )
 }
 
-export default GoalForm
+export default GoalItem
